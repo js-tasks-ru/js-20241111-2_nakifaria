0f80324654ed0a7d733331c6d07743e5e8d9d434
AnastasiaS <nakifaria@list.ru>
2024-11-18 12:34:31 +0300

    sort done

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,10 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  let newArr = [...arr];
+  if (param == 'desc') {
+    return newArr.sort((a, b) => b.localeCompare(a, ['ru', 'en'], { caseFirst: "upper" }));
+  }
+  return newArr.sort((a, b) => a.localeCompare(b, ['ru', 'en'], { caseFirst: "upper" }));
 }
+
