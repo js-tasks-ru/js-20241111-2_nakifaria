657b08ceaa9fdad0b875302c9779f0744e4e35bc
AnastasiaS <nakifaria@list.ru>
2024-11-25 16:26:58 +0300

    inver-object done

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,14 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  const invertedObj = {};
+  if (obj) {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        invertedObj[obj[key]] = key;
+      }
+    }
+    return invertedObj;
+  }
+  return undefined;
 }
