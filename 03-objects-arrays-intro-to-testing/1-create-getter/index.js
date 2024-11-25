25944188ba004053da0a61e1eb9383dcc6ab98db
AnastasiaS <nakifaria@list.ru>
2024-11-25 16:02:18 +0300

    create-getter done

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,13 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+export function createGetter(path) {
+  const result = path.split('.');
+  
+  return function(obj) {
+    return result.reduce((acc, part) => {
+      return acc ? acc[part] : undefined;
+    }, obj);
+  };
 }
