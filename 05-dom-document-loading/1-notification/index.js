515422bb14c89e1933e7f8397f0b26d0c7aea05f
AnastasiaS <nakifaria@list.ru>
2024-11-29 12:10:25 +0300

    6/8 of test done

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,44 @@
 export default class NotificationMessage {
+    element
+    constructor (text, {duration = 0, type = ""}) {
+      this.text = text;
+      this.duration = duration;
+      this.type = type;
+      this.element = this.createElement(this.createTemplate());
+    }
 
+    createElement(template) {
+      const element = document.createElement('div');
+      element.innerHTML = template;
+      return element.firstElementChild;
+    }
+
+    createTemplate() {
+      return (`
+    <div class="notification ${this.type}" style="--value:20s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.text}
+      </div>
+    </div>
+    </div>
+            `);
+    }
+
+    show() {
+      this.createElement(this.createTemplate());
+      setTimeout(() => {}, this.duration);
+      this.destroy();
+    }
+
+    remove () {
+      this.element.remove();
+    }
+
+    destroy() {
+      this.remove();
+      this.duration = 0;
+    }
 }
